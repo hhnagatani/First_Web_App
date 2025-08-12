0b0625e7372a7ded88b1d50791f8b94367aa3084
HOWARD H NAGATANI <hhnagatani@gmail.com>
2025-08-11 19:27:32 -0500

    Auto update table

---
+++ script/script.js
@@ -0,0 +1,28 @@
+
+const tableData = [
+    ["Finch",   "Harold", "programmer", "000-00-xxxx"],
+    ["Reeve",   "John",   "assassin",   "111-11-xxxx"],
+    ["Fusco",   "Lionel", "detective",  "222-22-xxxx"],
+    ["Morgan",  "Zoe",    "fixer",      "333-33-xxxx"],
+    ["Cause",   "root",   "angel",      "444-44-xxxx"],
+    ["Carter",  "Joss",   "detective",  "555-55-xxxx"],
+    ["Shah",    "Sameen", "assassin",   "666-66-xxxx"]
+];
+
+function loadTable(data, index) {
+    var tableBody = document.getElementById("tbody");
+
+    var row = tableBody.insertRow(index);
+
+    var cell0 = row.insertCell();
+    var cell1 = row.insertCell();
+    var cell2 = row.insertCell();
+    var cell3 = row.insertCell();
+    var cell4 = row.insertCell();
+
+    cell0.innerHTML = index;
+    cell1.innerHTML = data[index, 0];
+    cell2.innerHTML = data[index, 1];
+    cell3.innerHTML = data[index, 2];
+    cell4.innerHTML = data[index, 3];
+}
